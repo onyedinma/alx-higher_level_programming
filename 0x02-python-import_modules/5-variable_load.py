b4f7e8242e2d53e231c259ed7b56c3c00879ab7a
KachKreative <onyedinmaonyekachi@yahoo.com>
2023-01-06 05:38:01 +0100

    Update

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Print the value of variable a from variable_load_5."""
+    """print load variable"""
     from variable_load_5 import a
-
-    print(a)
+    print("{:d}".format(a))
