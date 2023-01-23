03a435334a55d593254c0ea5765f373d7d3e714e
KachKreative <onyedinmaonyekachi@yahoo.com>
2023-01-23 23:12:32 +0100

    Error Handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # 0-safe_print_list.py
-# Onyedinma Onyekachi <onyedinmaonyekachi@gmail.com>
+# Brennan D Baraban <375@holbertonschool.com>
+
 
 def safe_print_list(my_list=[], x=0):
     """Print x elememts of a list.
@@ -21,6 +22,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print("")
     return (ret)
-
-
-
