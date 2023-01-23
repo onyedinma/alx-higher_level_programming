847a4f2aab85a69d3a0e3ae3e77b07daecbb372a
KachKreative <onyedinmaonyekachi@yahoo.com>
2023-01-23 23:28:21 +0100

    pycodestyle

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,13 +4,23 @@
 
 
 def magic_calculation(a, b):
+    """
+    This function performs a calculation using the values of a and b.
+    """
+    # Initialize result to 0
     result = 0
-    for i in range(1,3):
+    # Loop through the range of 1 and 3 (not including 3)
+    for i in range(1, 3):
         try:
+            # If i is greater than a, raise an exception
             if i > a:
                 raise Exception("Too far")
             else:
+                # Add the result of (a ** b) / i to the result variable
                 result += (a ** b) / i
         except:
+            # If an exception is raised, add a + b to the result variable
             result += a + b
-    return (result)
+    # Return the final result
+    return result
+
