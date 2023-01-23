167d8b7438012c9be1898aca22230b51a7aeb52c
KachKreative <onyedinmaonyekachi@yahoo.com>
2023-01-23 23:07:26 +0100

    code to only print integers and skip other types of values in the list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 0-safe_print_list.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Onyedinma Onyekachi <onyedinmaonyekachi@gmail.com>
 
 
 def safe_print_list(my_list=[], x=0):
@@ -16,9 +16,11 @@ def safe_print_list(my_list=[], x=0):
     ret = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
-            ret += 1
+            if isinstance(my_list[i], int):
+                print("{}".format(my_list[i]), end=" ")
+                ret += 1
         except IndexError:
             break
     print("")
     return (ret)
+
