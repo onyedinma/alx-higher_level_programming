81039af4853aa444793f8358527520ad952ae0ed
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-03-14 13:21:35 -0700

    fixed sql errors

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -4,10 +4,11 @@
 -- name VARCHAR(256)
 -- The database name will be passed as an argument of the mysql command
 -- If the table first_table already exists, your script should not fail
--- You are not allowed to use the SELECT or SHOW statements
+-- You are not allowed to use the SELECT or SHOW statement
 
+USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS first_table (
-	id INT
-	name VARCHAR(256)
+    id INT,
+    name VARCHAR(256)
 );
 
