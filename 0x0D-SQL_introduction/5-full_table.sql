c90343811a19b1d6d25f3f9e40fde359014a2650
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-03-14 13:42:25 -0700

    commit

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,7 @@
 -- Here's a script that prints the full description of the table first_table in the hbtn_0c_0 database:
 
-SELECT column_name, data_type, character_maximum_length, is_nullable, column_default
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+
 
