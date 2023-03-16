e07e6937afc098610638869bbe507b1dcb028537
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-03-16 15:21:57 -0700

    0-privileges.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,7 +1,8 @@
 -- Here's a script that prints the full description of the table first_table in the hbtn_0c_0 database:
 
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SELECT CONCAT('Table: ', TABLE_NAME, '\n\n', 'Create Table: ', CREATE_STATEMENT, '\n', ENGINE, ' ', CHARSET, '\n')
+FROM information_schema.tables
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'
+AND TABLE_NAME = 'first_table';
 
 
