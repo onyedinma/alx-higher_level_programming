cb2bd39990da1a569a9f32db2abfdb6ce135a841
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-06-25 19:25:39 +0000

    2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const process = require('process');
+const argv = process.argv;
+// print process.argv
+if (argv.length < 3) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
