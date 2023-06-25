a2c5982ee08d81f577af747a3373b08da0cff592
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-06-25 20:20:35 +0000

    4-concat.js

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const process = require('process');
+const argv = process.argv;
+// print process.argv
+if (argv[2] == null) {
+  console.log('No argument');
+} else {
+  console.log(argv[2] + ' is ' + argv[3]);
+}
