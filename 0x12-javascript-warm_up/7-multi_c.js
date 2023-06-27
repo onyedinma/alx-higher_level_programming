883c84f33a18764515de7f649f8cb12d916f9002
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-06-27 10:41:59 +0000

    correction "empty block"

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,8 +5,7 @@ const argv = process.argv;
 let i;
 if (argv[2] === undefined) {
   console.log('Missing number of occurrences');
-} else if (argv[2] < 0) {
-  } else {
+} else {
   for (i = 0; i < Number(argv[2]); i++) {
     console.log('C is fun');
   }
