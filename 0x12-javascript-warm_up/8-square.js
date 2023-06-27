3d329342eb5b63ea1e1ae9ba0f2fd6b193cc6574
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-06-27 11:49:37 +0000

    Missing size

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,7 +7,7 @@ if (!Number(argv[2])) {
   console.log('Missing size');
 } else {
   for (let i = 0; i <= argv[2] - 1; i++) {
-    xNum = xNum + 'x';
+    xNum = xNum + 'X';
   }
   for (let j = 0; j <= argv[2] - 1; j++) {
     console.log(xNum);
