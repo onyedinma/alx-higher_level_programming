73cec625a8d39680d7c23c5c4e5824ae96b9416c
KACH KREATIVE <richboyyork@RICHs-MacBook.local>
2023-08-16 01:57:51 +0100

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,13 @@
 #!/usr/bin/node
 
-// Returns the number of occurrences of an element in a list
+// This script defines a function that returns the number of occurrences of an element in a list.
 
-exports.nbOccurences = function (list, searchElement) {
-  return list.filter(x => x === searchElement).length;
-};
+function nbOccurences (list, searchElement) {
+  // Filter the list to only include elements that are equal to the search element
+  const filteredList = list.filter(x => x === searchElement);
+
+  // Return the length of the filtered list, which is the number of occurrences of the search element
+  return filteredList.length;
+}
+// Export the nbOccurences function
+exports.nbOccurences = nbOccurences;
