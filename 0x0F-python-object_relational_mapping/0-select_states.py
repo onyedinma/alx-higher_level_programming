d56c6fe05fc88397c09e4ae6539260dc45b0b387
KachKreative <onyedinmaonyekachi@yahoo.com>
2023-08-28 06:30:05 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,17 @@
-# A module to select states
-import MySQLdb
-import sys
-def main():
-    """Select states from database."""
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+#!/usr/bin/python3
+""" A module to select states """
 if __name__ == "__main__":
-    main()
+    import MySQLdb
+    import sys
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3],
+                           charset="utf8")
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM states ORDER BY id ASC")
+    query_res = cur.fetchall()
+    for row in query_res:
+        print(row)
+    curs.close()
+    conn.close()
