472efdd867776e6e1df7452b0895e9c0273fd371
KachKreative <onyedinmaonyekachi@yahoo.com>
2023-08-28 06:45:36 +0100

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-""" A module to select states """
-if __name__ == "__main__":
-    import MySQLdb
-    import sys
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2],
-                           db=sys.argv[3],
-                           charset="utf8")
+"""
+Script that lists all states
+with name starting with 'N' from database hbtn_0e_0_usa
+Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
+"""
+import MySQLdb
+import sys
+
+if len(sys.argv) == 4:
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    # Grab all the states in my database starting with 'N'
+    cur.execute("SELECT * from states ORDER BY id")
     query_rows = cur.fetchall()
     for row in query_rows:
-        if row[1][0] == "N":
+        if row[1][0] == 'N':
             print(row)
     cur.close()
     conn.close()
+else:
+    print("Usage: ./1-filter_states.py <mysql username>\
+ <mysql password> <database name>")
