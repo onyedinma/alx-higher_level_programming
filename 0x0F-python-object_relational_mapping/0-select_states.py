b07a219d045b72f42c8e50a52d67a1818633bd66
KachKreative <onyedinmaonyekachi@yahoo.com>
2023-08-28 06:27:46 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,8 +9,7 @@ def main():
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-        charset="utf8"
-    )
+        charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
