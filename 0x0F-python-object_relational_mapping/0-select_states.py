af6944f8a1607fb798cb802c052373b7e30604a1
KachKreative <onyedinmaonyekachi@yahoo.com>
2023-08-28 06:03:45 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-""" A module to select states """
+""" Module to select states """
 if __name__ == "__main__":
     import MySQLdb
     import sys
-    conn = MySQLdb.connect(host="localhost",
+    conne = MySQLdb.connect(host="localhost",
                            port=3306, user=sys.argv[1],
                            passwd=sys.argv[2],
                            db=sys.argv[3],
                            charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
+    curs = conne.cursor()
+    curs.execute("SELECT * FROM states ORDER BY id ASC")
+    query_row_results = curs.fetchall()
+    for row in query_row_results:
         print(row)
-    cur.close()
-    conn.close()
+    curs.close()
+    conne.close()
