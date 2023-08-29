bdc8561a4f8acbf297ee1335d8bae37ef94687ac
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-08-28 18:08:42 -0700

    m

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""1-filter_states.py: a script that lists all states with a name starting with N
+"""1-filter_states.py: a script that lists all
+states with a name starting with N
 (upper N) from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
-# get the arguments
 username = sys.argv[1]
 password = sys.argv[2]
 database = sys.argv[3]
 
-# connect to the database
 db = MySQLdb.connect(
     host="localhost",
     port=3306,
@@ -18,11 +17,8 @@ db = MySQLdb.connect(
     passwd=password,
     db=database,
 )
-
-# create a cursor object
 cur = db.cursor()
 
-# execute the query
 cur.execute(
     """
     SELECT *
@@ -31,14 +27,10 @@ cur.execute(
     ORDER BY id ASC
     """
 )
-
-# fetch all the rows
 rows = cur.fetchall()
 
-# print each row
 for row in rows:
     print(row)
 
-# close the cursor and the database
 cur.close()
 db.close()
