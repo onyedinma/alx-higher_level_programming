51abf863729ffb4151669eb197973ceaa7c03ff6
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-08-29 11:36:02 -0700

    modified with function

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-"""Script to print states via sqlalchemy
+"""Script to add a state to the states table via sqlalchemy
 """
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
+def add_state(engine, state_name):
+    """Adds a state with the given name to the states table"""
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    louisiana = State(name=state_name)
+    session.add(louisiana)
+    session.commit()
+    print(louisiana.id)
+
+
 if __name__ == "__main__":
+    """ Usage: python3 script.py/
+    <db_username> <db_password> <db_name> <state_name>"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1],
         sys.argv[2],
         sys.argv[3]),
         pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    louis = State(name="Louisiana")
-    session.add(louis)
-    session.commit()
-    results = session.query(State)\
-        .filter_by(name="Louisiana").first()
-    print("{}".format(results.id))
+    state_name = sys.argv[4]
+    add_state(engine, state_name)
