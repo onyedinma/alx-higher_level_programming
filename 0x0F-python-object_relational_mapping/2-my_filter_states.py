b44b62e0109a51b7e27cf99e544a090282d1a673
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-08-28 18:30:52 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
-import sys
-import MySQLdb
-
+""" A module to filter states by name """
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host="localhost",
+                         port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name='{}' \
+            ORDER BY id ASC".format(sys.argv[4]))
+    states = cursor.fetchall()
+    for state in states:
+        if state[1] == sys.argv[4]:
+            print(state)
+    cursor.close()
+    db.close()
