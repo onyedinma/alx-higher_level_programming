fbf8be147ee9a990063191cad177f602db46cc17
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-08-28 17:56:13 -0700

    module doc

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-# 1-filter_states.py: a script that lists all
-# states with a name starting with N
-# (upper N) from the database hbtn_0e_0_usa
+"""1-filter_states.py: a script that lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
