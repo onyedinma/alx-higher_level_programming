fa4478dd59d3b85ba26c927d4aeff56244228bb6
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-08-28 18:13:01 -0700

    style error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,36 +1,18 @@
 #!/usr/bin/python3
-"""1-filter_states.py: a script that lists all
-states with a name starting with N
-(upper N) from the database hbtn_0e_0_usa"""
-
-import MySQLdb
-import sys
-
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=username,
-    passwd=password,
-    db=database,
-)
-cur = db.cursor()
-
-cur.execute(
-    """
-    SELECT *
-    FROM states
-    WHERE name LIKE 'N%'
-    ORDER BY id ASC
-    """
-)
-rows = cur.fetchall()
-
-for row in rows:
-    print(row)
-
-cur.close()
-db.close()
+""" A module to select states """
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1][0] == "N":
+            print(row)
+    cur.close()
+    conn.close()
