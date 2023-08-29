5691424a04f34f21e3a0c94ad7bd80632576bece
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-08-28 18:51:58 -0700

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
-# Displays all cities of a given state from the
-# states table of the database hbtn_0e_4_usa.
-# Safe from SQL injections.
-# Usage: ./5-filter_cities.py <mysql username> \
-#                             <mysql password> \
-#                             <database name> \
-#                             <state name searched>
-import sys
-import MySQLdb
+""" A module to list all cities of a state from a database
+
+Usage: python3 2-my_filter_states.py <username> <password> <database> <state>
+"""
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+    import MySQLdb
+    import sys
+    # connect to the database using the arguments
+    db = MySQLdb.connect(host="localhost",
+                         port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         charset="utf8")
+    # create a cursor object to execute queries
+    cursor = db.cursor()
+    # execute the query using a join between cities and states tables
+    cursor.execute("""
+        SELECT City FROM (
+        SELECT cities.id AS ID, cities.name AS City,
+        states.name AS State FROM cities
+        JOIN states
+        ON cities.state_id = states.id
+        ORDER BY cities.id ASC) as Table1
+        WHERE State='{}';
+        """.format(sys.argv[4]))
+    # fetch all the rows from the query result
+    cities = cursor.fetchall()
+    # print the city names separated by commas
+    print(", ".join(city[0] for city in cities))
+    # close the cursor and the database
+    cursor.close()
+    db.close()
