4259348e6f8911885e4b562b129350e2666c7dd6
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-08-29 10:50:33 -0700

    improved code

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
-# Lists the State object with the name passed as argument
-# from the database hbtn_0e_6_usa.
-# Usage: ./10-model_state_my_get.py <mysql username> /
-#                                   <mysql password> /
-#                                   <database name>
-#                                   <state name searched>
+"""Script to print states via sqlalchemy
+"""
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+
+def print_state_id(engine, state_name):
+    """Prints the id of the state with the given name"""
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    found = False
-    for state in session.query(State):
-        if state.name == sys.argv[4]:
-            print("{}".format(state.id))
-            found = True
-            break
-    if found is False:
+    result = session.query(State)\
+        .filter_by(name=state_name)\
+        .first()
+    if result:
+        print(result.id)
+    else:
         print("Not found")
+
+
+if __name__ == "__main__":
+    """ Usage: python3 script.py\
+    <db_username> <db_password> <db_name> <state_name>"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True)
+    state_name = sys.argv[4]
+    print_state_id(engine, state_name)
