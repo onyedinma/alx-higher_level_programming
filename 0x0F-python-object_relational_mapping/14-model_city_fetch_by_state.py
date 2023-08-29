5a1df84264323d1c182fbf6107f75464ff79e3f3
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-08-29 14:00:18 -0700

    commented

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-# Lists all City objects from the database hbtn_0e_14_usa.
-# Usage: ./14-model_city_fetch_by_state.py <mysql username> /
-#                                          <mysql password> /
-#                                          <database name>
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
-from model_city import City
+"""Print states and cities using SQLAlchemy."""
+from relationship_state import Base, State  # Import model classes
+from relationship_city import City  # Import City class
+from sqlalchemy import create_engine  # Import create_engine
+from sqlalchemy.orm import sessionmaker  # Import sessionmaker
+from sys import argv  # Import argv from sys module
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    # Create an SQLAlchemy engine to connect to the database
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]))  # Extract credentials from argv
+
+    Base.metadata.create_all(engine)  # Create tables if they don't exist
+
+    # Create a session factory for interacting with the database
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for city, state in session.query(City, State) \
-                              .filter(City.state_id == State.id) \
-                              .order_by(City.id):
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    # Query for cities and their corresponding states
+    result = session.query(City, State).filter(City.state_id == State.id).all()
+
+    for row in result:
+        # Print city information along with the corresponding state
+        print("{}: ({}) {}".format(row[1].name, row[0].id, row[0].name))
