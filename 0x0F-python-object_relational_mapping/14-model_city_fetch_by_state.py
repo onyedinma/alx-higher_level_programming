fff1f8ef62b8b902f3bff3a04ac7fe2a6cbc13be
KachKreative <onyedinmaonyekachi@yahoo.com>
2023-09-02 19:42:08 +0100

    update

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-"""Print states and cities using SQLAlchemy."""
-from relationship_state import Base, State  # Import model classes
-from relationship_city import City  # Import City class
-from sqlalchemy import create_engine  # Import create_engine
-from sqlalchemy.orm import sessionmaker  # Import sessionmaker
-from sys import argv  # Import argv from sys module
+"""A script to print all the states and cities """
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
 
 if __name__ == "__main__":
-    # Create an SQLAlchemy engine to connect to the database
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3]))  # Extract credentials from argv
-
-    Base.metadata.create_all(engine)  # Create tables if they don't exist
-
-    # Create a session factory for interacting with the database
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    # Query for cities and their corresponding states
     result = session.query(City, State).filter(City.state_id == State.id).all()
-
     for row in result:
-        # Print city information along with the corresponding state
         print("{}: ({}) {}".format(row[1].name, row[0].id, row[0].name))
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-# Defines a State model.
-# Inherits from SQLAlchemy Base and links to the MySQL table states.
+""" A module to define the state table using SQLAlchemy"""
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from relationship_city import Base, City
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
 
 
 class State(Base):
-    """Represents a state for a MySQL database.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        id (sqlalchemy.Integer): The state's id.
-        name (sqlalchemy.String): The state's name.
-        cities (sqlalchemy.orm.relationship): The State-City relationship.
-    """
+    """A class that maps to the states table"""
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-    cities = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship("City",
+                          backref="state",
+                          cascade="all, delete, delete-orphan")
