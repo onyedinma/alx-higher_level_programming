bf9f5c97adb1cde372d7453a2ce3778842e37f1e
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-09-02 11:50:59 -0700

    modifying relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -6,13 +6,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 
-if __name__ == "__main__":
+
+def print_states_and_cities():
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = State(name="California")
-    state.cities = [City(name="San Francisco")]
-    session.add(state)
-    session.commit()
+    states = session.query(State).all()
+    for state in states:
+        print(f"{state.name}:")
+        for city in state.cities:
+            print(f"\t{city.name}")
+    session.close()
+
+
+if __name__ == "__main__":
+    print_states_and_cities()
