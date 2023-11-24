b71bd5fac82a043b498c0b3184d9e3e76d12e00c
KachKreative <onyedinmaonyekachi@yahoo.com>
2023-11-24 05:19:21 +0100

    Delete 0x0D-SQL_introduction directory

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++
@@ -1,5 +0,0 @@
--- Write a script that creates the database hbtn_0c_0 in your MySQL server.
--- If the database hbtn_0c_0 already exists, your script should not fail
-
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
--- 0x0D-SQL_introduction/2-remove_database.sql
+++
@@ -1,4 +0,0 @@
--- a script that deletes the database hbtn_0c_0 in your MySQL server.
--- If the database hbtn_0c_0 doesn’t exist, your script should not fail
-DROP DATABASE IF EXISTS hbtn_0c_0; 
- 
