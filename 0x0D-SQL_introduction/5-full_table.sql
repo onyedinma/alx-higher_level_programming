0c0e603d885117155071dc59b81c107b55318ac7
KachKreative <onyedinmaonyekachi@yahoo.com>
2023-12-14 23:02:20 +0100

    corrected

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,14 +1,2 @@
-<<<<<<< HEAD
--- Here's a script that prints the full description of the table first_table in the hbtn_0c_0 database:
-
-SELECT CONCAT('Table: ', TABLE_NAME, '\n\n', 'Create Table: ', CREATE_STATEMENT, '\n', ENGINE, ' ', CHARSET, '\n')
-FROM information_schema.tables
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-AND TABLE_NAME = 'first_table';
-
-
-=======
--- print full description of the table first_table
--- from the database hbtn_0c_0
+-- Script that prints the full description of first_table from hbtn_0c_0
 SHOW CREATE TABLE first_table;
->>>>>>> af956157b46d2d02a78f90c00b0f4dfc6ce4d9d2
